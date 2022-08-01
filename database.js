21e611230aa3da46da8dcb934e801048e96e6ba4
Vatsav Sethupathi <vatsav14@outlook.com>
2022-08-01 21:53:49 +0530

    Connecting database to ClearDB

--- database.js
+++ database.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql2');
 var conn = mysql.createConnection({
-  host: 'localhost', // Replace with your host name
-  user: 'root',      // Replace with your database username
-  password: 'sql123',      // Replace with your database password
-  database: 'test' // // Replace with your database Name
+  host: 'us-cdbr-east-06.cleardb.net', // Replace with your host name
+  user: 'bbdaa91f3a4cd2',      // Replace with your database username
+  password: 'a79504d7',      // Replace with your database password
+  database: 'heroku_546a6ca300d9703' // // Replace with your database Name
 }); 
  
 conn.connect(function(err) {
