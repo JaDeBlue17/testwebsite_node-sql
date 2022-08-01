3d9c5734b2e1c1ee2a43b9e79158ae12ccbf2a5f
Vatsav Sethupathi <vatsav14@outlook.com>
2022-08-01 23:55:30 +0530

    'back'

--- database.js
+++ database.js
@@ -1,5 +1,5 @@
 var mysql = require('mysql2');
-var conn = mysql.createPool({
+var conn = mysql.createConnection({
   host: 'us-cdbr-east-06.cleardb.net', // Replace with your host name
   user: 'bbdaa91f3a4cd2',      // Replace with your database username
   password: 'a79504d7',      // Replace with your database password
