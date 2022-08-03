64999994fb613532ceb11ae646f923ef06fd0654
Vatsav Sethupathi <vatsav14@outlook.com>
2022-08-03 22:45:40 +0530

    'Pool_again'

--- database.js
+++ database.js
@@ -1,13 +1,13 @@
 var mysql = require('mysql2');
-var conn = mysql.createConnection({
+var conn = mysql.createPool({
   host: 'us-cdbr-east-06.cleardb.net', // Replace with your host name
   user: 'bbdaa91f3a4cd2',      // Replace with your database username
   password: 'a79504d7',      // Replace with your database password
   database: 'heroku_546a6ca300d9703' // // Replace with your database Name
 }); 
  
-conn.connect(function(err) {
-  if (err) throw err;
-  console.log('Database is connected successfully !');
-});
+//conn.connect(function(err) {
+//  if (err) throw err;
+//  console.log('Database is connected successfully !');
+//});
 module.exports = conn;
